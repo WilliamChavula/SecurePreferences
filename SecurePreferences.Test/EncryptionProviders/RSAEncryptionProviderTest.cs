59cf06c3854e7e7588840e9acf894f03e0922406
williamchavula <rumbani@gmail.com>
2025-01-18 11:30:49 -0600

    add tests for RSAEncryptionProvider

--- SecurePreferences.Test/EncryptionProviders/RSAEncryptionProviderTest.cs
+++ SecurePreferences.Test/EncryptionProviders/RSAEncryptionProviderTest.cs
@@ -1,17 +1,129 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
-
 namespace SecurePreferences.Test.EncryptionProviders
 {
+    public class RSAFixture
+    {
+        public string StringToEncrypt { get; init; }
+
+        public RSAFixture()
+        {
+            StringToEncrypt = "stringToEncrypt";
+        }
+    }
+
     public class RSAEncryptionProviderTest
     {
-        [Fact]
-        public void Test1()
+        public class RSAEncryptionTests(RSAFixture rSAFixture) : IClassFixture<RSAFixture>
         {
-            Assert.True(true);
+            [Fact]
+            public void ShouldEncryptPlainTextUsingRSAAlgorithm()
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var pubKey = rsaInstance.ExportPublicKey();
+
+                // When
+                var encrypted = rsaInstance.Encrypt(rSAFixture.StringToEncrypt, pubKey);
+
+                // Then
+                Assert.NotEmpty(pubKey);
+                Assert.NotEmpty(rSAFixture.StringToEncrypt);
+                Assert.NotEqual(rSAFixture.StringToEncrypt, encrypted);
+            }
+
+            [Theory]
+            [InlineData(null, "ae65fb42c9b261", "value cannot be null here.")]
+            [InlineData("gibberish", null, "key cannot be null here.")]
+            public void ShouldThrowArgumentExceptionGivenNullParameters(
+                string value,
+                string key,
+                string errorMsg
+            )
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var pubKey = rsaInstance.ExportPublicKey();
+
+                // When & Then
+                var exception = Assert.Throws<ArgumentNullException>(
+                    () => rsaInstance.Encrypt(value, key)
+                );
+                Assert.Contains(errorMsg, exception.Message);
+            }
+
+            [Fact]
+            public void ShouldThrowInvalidOperationExceptionWhenEncryptingWithNonRSAKey()
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var nonRSAKey = "ae65fb42c9b261e10729f94443e595fc91fdc10a";
+                var value = "gibberish";
+
+                // When & Then
+                var exception = Assert.Throws<InvalidOperationException>(
+                    () => rsaInstance.Encrypt(value, nonRSAKey)
+                );
+                Assert.Contains("Failed to encrypt", exception.Message);
+            }
+        }
+
+        public class RSADecryptionTests(RSAFixture rSAFixture) : IClassFixture<RSAFixture>
+        {
+            [Fact]
+            public void ShouldDecryptCipherTextUsingRSAAlgorithm()
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var pubKey = rsaInstance.ExportPublicKey();
+                var privateKey = rsaInstance.ExportPrivateKey();
+                var encrypted = rsaInstance.Encrypt(rSAFixture.StringToEncrypt, pubKey);
+
+                // When
+                var decrypted = rsaInstance.Decrypt(encrypted, privateKey);
+
+                // Then
+                Assert.NotEmpty(pubKey);
+                Assert.NotEmpty(privateKey);
+                Assert.Equal(rSAFixture.StringToEncrypt, decrypted);
+            }
+
+            [Theory]
+            [InlineData(null, "ae65fb42c9b261", "Cipher Text cannot be null here.")]
+            [InlineData("gibberish", null, "key cannot be null here.")]
+            public void ShouldThrowArgumentExceptionGivenNullParameters(
+                string value,
+                string key,
+                string errorMsg
+            )
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var pubKey = rsaInstance.ExportPublicKey();
+                var privateKey = rsaInstance.ExportPrivateKey();
+                var encrypted = rsaInstance.Encrypt(rSAFixture.StringToEncrypt, pubKey);
+
+                // When & Then
+                var exception = Assert.Throws<ArgumentNullException>(
+                    () => rsaInstance.Decrypt(value, key)
+                );
+                Assert.Contains(errorMsg, exception.Message);
+            }
+
+            [Fact]
+            public void ShouldThrowInvalidOperationExceptionWhenDecryptingWithNonRSAKey()
+            {
+                // Given
+                var rsaInstance = Encryption.RSA;
+                var nonRSAKey = "ae65fb42c9b261e10729f94443e595fc91fdc10a";
+                var pubKey = rsaInstance.ExportPublicKey();
+                var encrypted = rsaInstance.Encrypt(rSAFixture.StringToEncrypt, pubKey);
+
+                // When & Then
+                var exception = Assert.Throws<InvalidOperationException>(
+                    () => rsaInstance.Decrypt(encrypted, nonRSAKey)
+                );
+
+                Assert.Contains("Failed to decrypt", exception.Message);
+            }
         }
     }
 }
